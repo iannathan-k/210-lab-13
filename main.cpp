9dee6b06cd22091a17a8d1586aa3f03a1e341710
iannathan-k <ianmtv@gmail.com>
2026-02-19 16:08:29 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+#include <numeric>
+#include <fstream>
+
+using namespace std;
+
+// COMSC-210 | Lab 12 | Ian Kusmiantoro
+// This program tracks the number of points a team scored in a season
+
+int main() {
+    const int SIZE = 50;
+    const int SEARCH = 67;
+    array<int, SIZE> scores;
+
+    ifstream fin;
+    fin.open("scores.txt");
+
+    // Checking if file input stream is good
+    if (fin.good()) {
+
+        // Input data from file one line at a time
+        for (int i = 0; i < scores.size(); i++) {
+            fin >> scores.at(i);
+        } 
+
+        // Output before scores
+        cout << "===== Scores =====" << endl;
+        for (int score : scores) {
+            cout << score << " ";
+        }
+        cout << endl << endl;
+        
+        // General Statistical Data
+        cout << "First Game: " << scores.front() << endl;
+        cout << "Last Game: " << scores.back() << endl;
+        cout << "Best Score: " << *max_element(scores.begin(), scores.end()) << endl;
+        cout << "Worst Score: " << *min_element(scores.begin(), scores.end()) << endl;
+
+        // Calculate average score using accumulate from numeric
+        int total = accumulate(scores.begin(), scores.end(), 0);
+        cout << "Average Score: " << static_cast<double>(total) / scores.size() << endl;
+
+        // Sort using algorithm
+        cout << endl;
+        sort(scores.begin(), scores.end());
+        cout << "===== Sorted Scores =====" << endl;
+        for (int score : scores) {
+            cout << score << " ";
+        }
+        cout << endl << endl;
+
+        cout << "Median Score: " << scores.at(scores.size() / 2) << endl;
+
+        // Find using iterator and algorithm
+        array<int, SIZE>::iterator it;
+        it = find(scores.begin(), scores.end(), SEARCH);
+
+        // If the iterator didn't reach the end (last element + 1), it managed to find the element
+        if (it != scores.end()) {
+            // Broken up into 2 to keep it from being too long
+            cout << "Team scored " << SEARCH << " points on game ";
+            cout << it - scores.begin() << "!" << endl;
+        } else {
+            cout << "Team did not ever score " << SEARCH << " points on any game. :(" << endl;
+        }
+
+        // Wipe the scores for the next season!
+        scores.fill(0);
+        cout << endl;
+        cout << "===== Wiped Scores =====" << endl;
+        for (int score : scores) {
+            cout << score << " ";
+        }
+        cout << endl << endl;
+
+        fin.close();
+    } else {
+        cout << "Error in accessing the file!" << endl;
+    }
+
+    return 0;
+}
